010f5775620b01868e0b6ddd5f1d1b264fba812b
Sebastian <sebastian.sergelius@helsinki.fi>
2019-01-19 17:27:57 +0200

    autentikointia korjattu

--- application/auth/models.py
+++ application/auth/models.py
@@ -11,6 +11,7 @@ class User(db.Model):
     username = db.Column(db.String(144), nullable=False)
     password = db.Column(db.String(144), nullable=False)
 
+    kirjaus = db.relationship("Kirjaus", backref='account', lazy=True)
     def __init__(self, name, username, password):
         self.name = name
         self.username = username
