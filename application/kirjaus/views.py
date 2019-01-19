f9081875e9546e9614514f9bd6eb5a0c2d28cc71
Sebastian <sebastian.sergelius@helsinki.fi>
2019-01-19 16:55:44 +0200

    lisätty login_required toiminnallisuus

--- application/kirjaus/views.py
+++ application/kirjaus/views.py
@@ -1,18 +1,22 @@
 from application import app, db
+from flask_login import login_required
 from flask import render_template, request, redirect, url_for
 from application.kirjaus.models import Kirjaus
 from application.kirjaus.forms import KirjausForm
 from datetime import datetime, time, date
 
 @app.route("/kirjaus", methods=["GET"])
+@login_required
 def kirjaus_index():
     return render_template("kirjaus/list.html", kirjauslista = Kirjaus.query.all())
 
 @app.route("/kirjaus/new/")
+@login_required
 def kirjaus_form():
     return render_template("kirjaus/new.html", form = KirjausForm())
 
 @app.route("/kirjaus/<kirjaus_id>/", methods=["POST"])
+@login_required
 def kirjaus_uloskirjaus(kirjaus_id):
     kirjaus = Kirjaus.query.get(kirjaus_id)
     now = datetime.datetime.now()
@@ -22,6 +26,7 @@ def kirjaus_uloskirjaus(kirjaus_id):
     return redirect(url_for("kirjaus_index"))
 
 @app.route("/kirjaus/", methods=["POST"])
+@login_required
 def kirjaus_create():
     form = KirjausForm(request.form)
     #sisaankirjausPvm = datetime.datetime.strptime(form.aika.data + ' ' + form.time.data, "%Y-%m-%d %H:%M")
