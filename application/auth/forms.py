8c963577a2fc0990a83a91662df5e0b2468874c7
Sebastian <sebastian.sergelius@helsinki.fi>
2019-01-24 15:18:00 +0200

    rekisteröintilomake tehty

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
 
 class LoginForm(FlaskForm):
-    username = StringField("Käyttäjätunnus")
-    password = PasswordField("Salasana")
+    username = StringField("Käyttäjätunnus", [validators.Required()])
+    password = PasswordField("Salasana", [validators.Required()])
+
+    class Meta:
+        csrf = False
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Uusi tunnus", [validators.Required()])
+    password = PasswordField("Salasana", [validators.Required()])
+    name = StringField("Nimi", [validators.Required()])
 
     class Meta:
         csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,11 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
 
-from application import app
+from sqlalchemy.exc import IntegrityError
+
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegistrationForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -23,4 +25,28 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
+    return redirect(url_for("index"))
+
+@app.route("/auth/register")
+@login_required
+def auth_register():
+    return render_template("auth/registration.html", form = RegistrationForm())
+
+@app.route("/auth/createuser", methods=["POST"])
+@login_required
+def auth_create_user():
+    form = RegistrationForm(request.form)
+
+    if not form.validate():
+        return render_template("auth/registration.html", form = form)
+
+    user = User(form.name.data, form.username.data, form.password.data)
+    db.session().add(user)
+
+    try:
+        db.session().commit()
+    except IntegrityError:
+        db.session().rollback()
+        return render_template("auth/registration.html", form = form, error = "Käyttäjätunnus varattu, valitse toinen käyttäjätunnus")
+
     return redirect(url_for("index"))
