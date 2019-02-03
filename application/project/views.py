286ce2d9a93f93236fc0055f55dc59d51e4d3292
Sebastian <sebastian.sergelius@helsinki.fi>
2019-02-03 18:14:45 +0200

    projektiin liittäminen käyttäjää luotaessa

--- application/project/views.py
+++ application/project/views.py
@@ -1,22 +1,32 @@
-from application import app, db
-from flask_login import login_required, current_user
+from application import app, db, login_required
+from flask_login import current_user
 from flask import render_template, request, redirect, url_for
 
 from application.project.models import Projekti
 from application.project.forms import ProjectForm
 
 @app.route("/project/new/")
-@login_required
+@login_required(role="ADMIN")
 def project_form():
-    return render_template("project/new.html", form = ProjectForm())
+    projektit = Projekti.query.all() 
+    return render_template("project/new.html", projektit = projektit, form = ProjectForm())
+
+
+@app.route("/project/delete/<project_id>", methods=["POST"])
+@login_required(role="ADMIN")
+def projekti_poista(project_id):
+    Projekti.query.filter_by(id = project_id).delete()
+    db.session().commit()
+    return redirect(url_for("project_form"))
+
 
 @app.route("/project/create/", methods=["POST"])
-@login_required
+@login_required(role="ADMIN")
 def project_create():
     form = ProjectForm(request.form)
 
     if not form.validate():
-        return render_template("project/new.html", form = form)
+        return render_template("project/new.html", form = form, error = "Varmista, että työaika on kokonaisluku")
 
     project = Projekti(form.name.data, form.customer.data, form.vakiotyoaika.data)
 
