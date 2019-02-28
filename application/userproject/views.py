eae34dd4b71600b4a47ff6856d3fea0ad3b2b88d
Sebastian <sebastian.sergelius@helsinki.fi>
2019-02-28 11:59:53 +0200

    siistitty koodia ja tehty metodeja avuksi

--- application/userproject/views.py
+++ application/userproject/views.py
@@ -12,23 +12,31 @@ from application.userproject.models import generate_form, paivita_kayttaja, kayt
 from application.userproject.forms import UserProjectForm
 from application.auth.models import User
 
-@app.route("/userproject/add/")
-@login_required(role="ADMIN")
-def userproject_form():
-    if current_user.role == "ADMIN":
-       
+def get_users_in_project():
+    if current_user.role == "ADMIN": 
         raportti = get_users_per_project()
     else:
         raportti = None
-    return render_template("userproject/add.html", raportti = raportti, form = generate_form(), kayttajat = get_users_w_project())
+    return raportti
+
+def get_add_html_site(message, kayttajat):
+    if message == None:
+        return render_template("userproject/add.html", raportti = get_users_in_project(), form = generate_form(), kayttajat = kayttajat)
+    else:
+        return render_template("userproject/add.html", error=message, raportti = get_users_in_project(), form = generate_form(), kayttajat = kayttajat)
+
+
+@app.route("/userproject/add/")
+@login_required(role="ADMIN")
+def userproject_form():
+    return get_add_html_site(None, get_users_w_project())
 
 @app.route("/userproject/settings/", methods=["GET"])
 @login_required()
 def kayttaja_asetukset():
     if request.method == "GET":
         res = hae_kirjautuneen_kayttajat_nakyma()
-        form = generate_form()
-        return render_template("userproject/add.html", form = form, kayttajat = res)
+        return get_add_html_site(None, res)
 
 @app.route("/userproject/linkuser/", methods=["POST"])
 @login_required(role="ANY")
@@ -38,32 +46,38 @@ def userproject_create():
         userproject = Userproject(form.asiakas.data)
         userproject.account_id = form.users.data
         userproject.project_id = form.project.data
-        if(form.paaprojekti.data == True):
-            tarkista_paaprojekti_ja_vaihda(form.users.data)
-        if(form.asiakas.data == True):
-            kayttajan_rooli_asiakkaaksi(form.users.data)
-        userproject.paaprojekti = form.paaprojekti.data
-        
+        userproject.paaprojekti = form.paaprojekti.data 
         db.session().add(userproject)
         try:
             db.session().commit()
         except IntegrityError:
             db.session.rollback()
-            return render_template("userproject/add.html", form = generate_form(), error = "Käyttäjä on jo liitetty projektiin", kayttajat = get_users_w_project())
+            message = "Käyttäjä on jo liitetty projektiin"
+            return get_add_html_site(message, get_users_w_project())
+
+        message = "Käyttäjä liitetty projektiin onnistuneesti!"
+        return get_add_html_site(message, get_users_w_project())
+
     elif request.form['action'] == "Päivitä":
         stmt = text("SELECT * FROM userproject WHERE account_id = :accountid AND project_id = :projectid").params(accountid = form.users.data, projectid = form.project.data)
         res = db.engine.execute(stmt)
         row = res.fetchone()
-        if row != None:
-            res.close()
+        res.close()
+        #jos käyttäjä liitettynä projektiin
+        if row != None: 
             tarkista_paaprojekti_ja_vaihda(form.users.data)
             paivita_kayttaja(form.users.data, form.project.data, form.paaprojekti.data, form.asiakas.data)
         else:
+            #liitä käyttäjä projektiin
             if current_user.role == "ADMIN":
-                return render_template("userproject/add.html", form = generate_form(), error = "Liitä käyttäjä ensiksi projektiin.", kayttajat=get_users_w_project())
-            else:
-                return render_template("userproject/add.html", form = generate_form(), kayttajat = hae_kirjautuneen_kayttajat_nakyma(), error = "Liitä itsesi ensiksi projektiin.")
-    if current_user.role == "ADMIN":
-        return render_template("userproject/add.html", form = generate_form(), error = 'Käyttäjä liitetty projektiin onnistuneesti!', kayttajat=get_users_w_project())
-    return render_template("userproject/add.html", form = generate_form(), kayttajat = hae_kirjautuneen_kayttajat_nakyma(), error = 'Ensisijainen projekti vaihdettu')
+                message = "Liitä käyttäjä ensiksi projektiin."
+                return get_add_html_site(message, get_users_w_project())
+            message = "Liitä itsesi ensiksi projektiin."
+            return get_add_html_site(message, hae_kirjautuneen_kayttajat_nakyma())
+        
+        #liitos onnistunut
+        if current_user.role == "ADMIN":
+            return get_add_html_site('Ensisijainen projekti vaihdettu', get_users_w_project())
+        return get_add_html_site('Ensisijainen projekti vaihdettu', hae_kirjautuneen_kayttajat_nakyma())
+    
 
